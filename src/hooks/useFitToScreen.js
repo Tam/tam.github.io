2e3e91bfdb52c040dfa0ef246609046eff6d4e0e
Tam <hi@tam.sx>
2021-01-24 18:32:22 +0000

    Add js height fit

--- src/hooks/useFitToScreen.js
+++ src/hooks/useFitToScreen.js
@@ -1,7 +1,17 @@
 import { useEffect } from 'react';
+import debounce from '../helpers/debounce';
 
 export default function useFitToScreen () {
 	useEffect(() => {
-		//
+		const onResize = debounce(() => {
+			const vh = window.innerHeight * 0.01;
+			document.documentElement.style.setProperty('--vh', `${vh}px`);
+		}, 15);
+
+		onResize();
+
+		window.addEventListener('resize', onResize);
+
+		return () => window.removeEventListener('resize', onResize);
 	}, []);
 }
